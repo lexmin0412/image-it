576d08b8d9f7e7f175fefe796576a72c98734ea3
lexmin0412 <zhangle_dev@outlook.com>
2022-07-30 02:43:07 +0800

    feat: add vconsole and less

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import VConsole from 'vconsole';
 import App from './App'
 import './index.css'
+
+new VConsole();
+console.log('ua', navigator.userAgent)
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
