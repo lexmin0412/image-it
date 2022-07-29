6ea5733f424d025b048e04897f2457fd5c4d79a0
lexmin0412 <zhangle_dev@outlook.com>
2022-07-30 01:01:29 +0800

    fix: 更换vite logo url为官网url]

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,7 @@ function App() {
     <div className="App">
       <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
+					<img src="https://vitejs.cn/logo.svg" className="logo" alt="Vite logo" />
         </a>
         <a href="https://reactjs.org" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
